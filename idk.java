f67d439fd76d87bce94eca1e0a24a368dfa3fb4a
LttleAnt <90708961+LttleAnt@users.noreply.github.com>
2021-09-14 08:19:13 -0700

    Create idk.java

---
+++ idk.java
@@ -0,0 +1,11 @@
+public class idk{
+  public static void main(String[] args){
+    int a = 3;
+    int b = 7;
+    int c = 9;
+    int [] arr = {a,b,c};
+    for(int arrsp = 0; arrsp <3; arrsp++;){
+      Sytem.out.println(arr[arrsp]);
+    }// end for 
+  }// end main
+}// end class
